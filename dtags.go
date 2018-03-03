ebe694cf72137d371699ee11225fb4e661cd5d75
tylerfowle <tylerfowle@gmail.com>
2018-03-03 00:27:19 -0600

    add more functions

--- dtags.go
+++ dtags.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/boltdb/bolt"
 	cmdline "github.com/galdor/go-cmdline"
+	"github.com/ryanuber/columnize"
 )
 
 type database struct {
@@ -32,7 +33,7 @@ func main() {
 	info.db = info.dbDir + info.dbFile
 
 	// make the path to the db file
-	os.MkdirAll(info.db, os.ModePerm)
+	os.MkdirAll(info.dbDir, os.ModePerm)
 
 	// ##################################################
 	// Command Line
@@ -43,6 +44,7 @@ func main() {
 	cmdline.AddCommand("del", "delete tag")
 	cmdline.AddCommand("get", "get tag")
 	cmdline.AddCommand("list", "list all tags")
+	cmdline.AddCommand("shell", "list all tags")
 
 	cmdline.Parse(os.Args)
 
@@ -52,9 +54,14 @@ func main() {
 	case "del":
 		deleteKeyFromDatabase(cmdline.CommandArgumentsValues(), info)
 	case "get":
-		getKeyFromDatabase(cmdline.CommandArgumentsValues(), info)
+		getPathFromTag(cmdline.CommandArgumentsValues(), info)
 	case "list":
 		listAllKeysInDatabase(cmdline.CommandArgumentsValues(), info)
+	case "shell":
+		shell(cmdline.CommandArgumentsValues(), info)
+	default:
+		//  TODO:  <03-03-18, yourname> // default command should search tags and open shell
+		shell(cmdline.CommandArgumentsValues(), info)
 	}
 
 }
@@ -100,9 +107,26 @@ func addKeyToDatabase(args []string, info database) {
 
 func deleteKeyFromDatabase(args []string, info database) {
 	fmt.Printf("running command \"del\" with arguments %v\n", args)
+
+	db, err := bolt.Open(info.db, 0666, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// tag
+	key := []byte(args[0])
+
+	// Delete the key in a different write transaction.
+	if err := db.Update(func(tx *bolt.Tx) error {
+		return tx.Bucket([]byte(info.bucket)).Delete([]byte(key))
+	}); err != nil {
+		log.Fatal(err)
+	}
+
 }
 
-func getKeyFromDatabase(args []string, info database) string {
+func getPathFromTag(args []string, info database) string {
 	fmt.Printf("getting %v from database\n", args)
 
 	// tag
@@ -152,12 +176,61 @@ func listAllKeysInDatabase(args []string, info database) {
 		b := tx.Bucket([]byte(info.bucket))
 		c := b.Cursor()
 
+		var unformattedList []string
 		for k, v := c.First(); k != nil; k, v = c.Next() {
-			fmt.Printf("key=%s, value=%s\n", k, v)
+			unformattedList = append(unformattedList, fmt.Sprintf("%s|%s\n", k, v))
 		}
+
+		formattedList := columnize.SimpleFormat(unformattedList)
+		fmt.Println(formattedList)
 
 		return nil
 
 	})
 
+}
+
+func shell(args []string, info database) {
+
+	// Get the current user.
+	me, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get the current working directory.
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cwd = getPathFromTag(args, info)
+
+	// Set an environment variable.
+	os.Setenv("SOME_VAR", "1")
+
+	// Transfer stdin, stdout, and stderr to the new process
+	// and also set target directory for the shell to start in.
+	pa := os.ProcAttr{
+		Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
+		Dir:   cwd,
+	}
+
+	// Start up a new shell.
+	// Note that we supply "login" twice.
+	// -fpl means "don't prompt for PW and pass through environment."
+	fmt.Print(">> Starting a new interactive shell")
+	proc, err := os.StartProcess("/usr/bin/login", []string{"login", "-fpl", me.Username}, &pa)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Wait until user exits the shell
+	state, err := proc.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Keep on keepin' on.
+	fmt.Printf("<< Exited shell: %s\n", state.String())
 }
